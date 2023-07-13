e783d8cdb026afafab72db6cfc6ff9cc9bb06fc0
andrewbantly <andrewbantly@gmail.com>
2023-07-12 20:53:54 -0700

    index

--- data-types/list.py
+++ data-types/list.py
@@ -13,3 +13,15 @@ Example:
 """
 
 ### Methods
+
+# INDEX
+"""
+list.index(x) - return zero-based index in the list of the first item whose value is equal to x. Also can use parameters to start the search further in the list list.index(element, index start, index end)
+"""
+nums_list = [3, 6, 9, 6, 15, 9, 6, 3]
+nums_list.index(6)
+# output: 1
+nums_list.index(6, 5)
+# output: 6
+nums_list.index(6, 2, 5)
+# output: 3
