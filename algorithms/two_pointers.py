ea959f4e2ade82422174c726b03dc5a948b89e2c
andrewbantly <andrewbantly@gmail.com>
2023-07-17 18:14:52 -0700

    two pointers starting description

---
+++ algorithms/two_pointers.py
@@ -0,0 +1,8 @@
+"""
+Two pointers is an effective technique that is typically used for searching pairs in a sorted array.
+
+A pointer is a reference to an object. 
+
+When might we use it? 
+Problems that involve collections such as arrays or lists, when we have to analyze each element of the collection compared to its other elements. 
+"""
